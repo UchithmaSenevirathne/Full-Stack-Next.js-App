0ed4a6222f1267fc5b9d5d2dc9ff029ef17ec9a0
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-01 19:43:52 +0530

    display posts

--- pages/index.js
+++ pages/index.js
@@ -4,30 +4,41 @@ import Hero from "@/components/Home/Hero";
 import Search from "@/components/Home/Search";
 import GameList from "@/components/Home/GameList";
 import app from "@/shared/FirebaseConfig";
-import { getFirestore, doc, setDoc, getDoc, 
-  collection, getDocs, query, where } from "firebase/firestore";
+import { useEffect, useState } from 'react'
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import Posts from "@/components/Home/Posts";
 
 export default function Home() {
-  const db=getFirestore(app);
+  const db = getFirestore(app);
 
-  const [posts,setPosts]=useState([])
-  useEffect(()=>{
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
     getPost();
-  },[])
+  }, []);
 
-  const getPost=async()=>{
+  const getPost = async () => {
     const querySnapshot = await getDocs(collection(db, "posts"));
     querySnapshot.forEach((doc) => {
-    
-   setPosts(posts=>[...posts,doc.data()]);
-});
-  }
+      setPosts((posts) => [...posts, doc.data()]);
+    });
+  };
 
   return (
     <div className="px-5 sm:px-7 md:px-10 mt-9">
-        <Hero/>
-        <Search/>
-        <GameList/>
+      <Hero />
+      <Search />
+      <GameList />
+      {posts? <Posts posts={posts}/>:null}
     </div>
-  )
+  );
 }
