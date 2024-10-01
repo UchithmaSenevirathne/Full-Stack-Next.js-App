56c4e5d5c1cfd95fb7a3b62d393e905306c308b3
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-01 15:56:08 +0530

    add styles

--- components/Header.js
+++ components/Header.js
@@ -4,13 +4,13 @@ const USER_IMAGE='https://res.cloudinary.com/dknvsbuyy/image/upload/v1686314044/
 
 function Header() {
   return (
-    <div>
+    <div className='flex justify-between p-5'>
         <img src='./Images/logo.png' width={150}/>
-        <div>
+        <div className='flex gap-4'>
             <button className='bg-black p-2 px-3 text-white rounded-full'>CREATE POST</button>
             <button className='bg-white text-gray-500 p-2 px-3 border-[1px] rounded-full'>SIGN IN</button>
+            <Image src={USER_IMAGE} width={40} height={40} className='rounded-full'/>
         </div>
-        <Image src={USER_IMAGE} width={40} height={40}/>
     </div>
   )
 }
