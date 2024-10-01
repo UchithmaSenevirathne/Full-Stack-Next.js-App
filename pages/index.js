538e68b5faac41d7cf90d1f5e2aed48cd22d2ff4
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-01 19:35:21 +0530

    get posts

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,25 @@ import styles from "@/styles/Home.module.css";
 import Hero from "@/components/Home/Hero";
 import Search from "@/components/Home/Search";
 import GameList from "@/components/Home/GameList";
+import app from "@/shared/FirebaseConfig";
 import { getFirestore, doc, setDoc, getDoc, 
   collection, getDocs, query, where } from "firebase/firestore";
-  
+
 export default function Home() {
   const db=getFirestore(app);
+
+  const [posts,setPosts]=useState([])
+  useEffect(()=>{
+    getPost();
+  },[])
+
+  const getPost=async()=>{
+    const querySnapshot = await getDocs(collection(db, "posts"));
+    querySnapshot.forEach((doc) => {
+    
+   setPosts(posts=>[...posts,doc.data()]);
+});
+  }
 
   return (
     <div className="px-5 sm:px-7 md:px-10 mt-9">
