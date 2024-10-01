c859fade90575ff1c55fc7ec527cd7ddc9a90853
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-01 19:48:19 +0530

    create post

--- components/Home/Posts.js
+++ components/Home/Posts.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Posts() {
   return (
-    <div>Posts</div>
+    <div>
+    <PostModal post={post} />
+    <div
+      className="grid grid-cols-1 
+  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4
+  gap-5 mt-5 px-10"
+   >
+      {posts.map((item,index) => (
+        <div key={index} onClick={()=>
+        {window.my_modal_1.showModal();setPost(item)}}>
+        <PostItem post={item} modal={true} />
+        </div>
+      ))}
+    </div>
+  </div>
   )
 }
 
