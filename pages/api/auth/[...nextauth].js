39b1ea108d81f803c939ec6546781469951b7fdb
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-01 16:17:08 +0530

    crete auth js

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth"
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    
+  ],
+}
+
+export default NextAuth(authOptions)
