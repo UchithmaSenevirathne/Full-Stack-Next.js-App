122c6679c2f24f00121da4593a09191ba01c1936
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-01 19:49:29 +0530

    display posts

--- components/Home/Posts.js
+++ components/Home/Posts.js
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-function Posts() {
+function Posts({ posts }) {
+  const [post, setPost] = useState();
+  useEffect(() => {
+    console.log("Posts", posts);
+  });
   return (
     <div>
-    <PostModal post={post} />
-    <div
-      className="grid grid-cols-1 
+      <PostModal post={post} />
+      <div
+        className="grid grid-cols-1 
   sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4
   gap-5 mt-5 px-10"
-   >
-      {posts.map((item,index) => (
-        <div key={index} onClick={()=>
-        {window.my_modal_1.showModal();setPost(item)}}>
-        <PostItem post={item} modal={true} />
-        </div>
-      ))}
+      >
+        {posts.map((item, index) => (
+          <div
+            key={index}
+            onClick={() => {
+              window.my_modal_1.showModal();
+              setPost(item);
+            }}
+          >
+            <PostItem post={item} modal={true} />
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Posts
+export default Posts;
