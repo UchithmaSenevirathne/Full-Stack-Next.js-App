a8b4752b1a195bfcb81b64bf182f8fc3eb2ae555
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-01 19:32:54 +0530

    Initialize Cloud Firestore and get a reference to the service

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,12 @@ import styles from "@/styles/Home.module.css";
 import Hero from "@/components/Home/Hero";
 import Search from "@/components/Home/Search";
 import GameList from "@/components/Home/GameList";
-
+import { getFirestore, doc, setDoc, getDoc, 
+  collection, getDocs, query, where } from "firebase/firestore";
+  
 export default function Home() {
+  const db=getFirestore(app);
+
   return (
     <div className="px-5 sm:px-7 md:px-10 mt-9">
         <Hero/>
