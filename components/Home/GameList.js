66de36a0ac5edf518afcdef06a6ac6c38ac71bef
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-01 18:38:52 +0530

    create game list

--- components/Home/GameList.js
+++ components/Home/GameList.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 function GameList() {
   return (
-    <div>GameList</div>
+    <div className='grid grid-cols-3 sm:grid-cols-4
+    md:grid-cols-5 lg:grid-cols-7 mt-4'>
+        {games?.map((item)=>(
+            <div key={item.id}
+              onClick={()=>onGamePress(item.name)}
+            className='flex flex-col 
+            items-center cursor-pointer
+            '>
+                <img src={item.image} 
+                width={45} height={45} 
+                className='hover:animate-bounce transition-all 
+                duration-150'/>
+                <h2 className='text-[14px] text-center'>{item.name}</h2>
+            </div>
+        ))}
+    </div>
   )
 }
 
