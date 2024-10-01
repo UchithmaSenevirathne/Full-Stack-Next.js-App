18a89b700cb337ed0371313e9290f604d7bd2c72
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-01 15:53:28 +0530

    add styles to buttons

--- components/Header.js
+++ components/Header.js
@@ -5,10 +5,10 @@ const USER_IMAGE='https://res.cloudinary.com/dknvsbuyy/image/upload/v1686314044/
 function Header() {
   return (
     <div>
-        <img src='./Images/logo.png'/>
+        <img src='./Images/logo.png' width={150}/>
         <div>
-            <button>CREATE POST</button>
-            <button>SIGN IN</button>
+            <button className='bg-black p-2 px-3 text-white rounded-full'>CREATE POST</button>
+            <button className='bg-white text-gray-500 p-2 px-3 border-[1px] rounded-full'>SIGN IN</button>
         </div>
         <Image src={USER_IMAGE} width={40} height={40}/>
     </div>
