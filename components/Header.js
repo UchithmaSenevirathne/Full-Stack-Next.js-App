dc60bf6f16c9b72632cea98af378765d0f8fc095
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-01 17:43:54 +0530

    add session

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,24 @@
 import React from "react";
 import Image from "next/image";
+import { useSession, signIn, signOut } from "next-auth/react";
 import { HiArrowLeftOnRectangle, HiOutlinePencilSquare } from "react-icons/hi2";
 const USER_IMAGE =
   "https://res.cloudinary.com/dknvsbuyy/image/upload/v1686314044/1617826370281_30f9a2a96a.jpg";
 
 function Header() {
+  const { data: session } = useSession();
+  console.log("Session", session);
   return (
     <div className="flex justify-between p-3 border-b-[2px] border-[#ff3366]">
       <img src="./Images/logo.png" width={150} />
       <div className="flex gap-4">
         <button className="bg-black p-2 px-3 text-white rounded-full">
           <span className="hidden sm:block ">CREATE POST</span>
-          <HiOutlinePencilSquare className="sm:hidden text-[20px]"/>
+          <HiOutlinePencilSquare className="sm:hidden text-[20px]" />
         </button>
         <button className="bg-white text-gray-500 p-2 px-3 border-[1px] rounded-full">
-        <span className="hidden sm:block ">SIGN IN</span>
-        <HiArrowLeftOnRectangle className="sm:hidden text-[20px]"/>
+          <span className="hidden sm:block ">SIGN IN</span>
+          <HiArrowLeftOnRectangle className="sm:hidden text-[20px]" />
         </button>
         <Image
           src={USER_IMAGE}
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
     <div>
-      <Header />
-      <Component {...pageProps} />;
-      <Footer />
+      <SessionProvider session={session}>
+        <Header />
+        <Component {...pageProps} />;
+        <Footer />
+      </SessionProvider>
     </div>
   );
 }
